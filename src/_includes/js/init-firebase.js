66092ae412e3a02efd4e5b4c80f33cbc8b38ac2e
Boopathi Rajaa <me@boopathi.in>
2020-03-07 19:08:48 +0100

    add firebase sdks

---
+++ src/_includes/js/init-firebase.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhfRgeSlggR1DkA_bxgQU1jcEJW9NLYcQ",
+  authDomain: "boopathi-blog.firebaseapp.com",
+  databaseURL: "https://boopathi-blog.firebaseio.com",
+  projectId: "boopathi-blog",
+  storageBucket: "boopathi-blog.appspot.com",
+  messagingSenderId: "452500282162",
+  appId: "1:452500282162:web:a7205cf1b1185ed44c952b",
+  measurementId: "G-FBDLWY9VFC"
+};
+
+function initFirebase() {
+  firebase.initializeApp(firebaseConfig);
+  window.firebasePerf = firebase.performance();
+}
+
+if (firebase) {
+  initFirebase();
+}
